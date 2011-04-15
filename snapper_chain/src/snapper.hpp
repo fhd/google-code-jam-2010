150b7f71306fc71f400a8d5387a17a02afe625c3
Felix H. Dahlke <fhd@ubercode.de>
2011-04-15 16:50:26 +0200

    Declared snapper functions as inline.

--- snapper_chain/src/snapper.hpp
+++ snapper_chain/src/snapper.hpp
@@ -1,10 +1,10 @@
-int snaps_required_for(int snappers) {
+inline int snaps_required_for(int snappers) {
    if (snappers == 1)
       return 1;
    return snaps_required_for(snappers - 1) * 2 + 1;
 }
 
-bool is_light_bulb_on(int snappers, int snaps)
+inline bool is_light_bulb_on(int snappers, int snaps)
 {
    int snaps_required = snaps_required_for(snappers);
    for (; snaps > snaps_required; snaps -= snaps_required + 1);
