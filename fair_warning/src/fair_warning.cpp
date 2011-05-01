9629831294693c3b5ea53f8d50ea1d1fadbb9bfe
Felix H. Dahlke <fhd@ubercode.de>
2011-05-01 19:30:27 +0200

    Replaced algorithm with a more sensible one.

--- fair_warning/src/fair_warning.cpp
+++ fair_warning/src/fair_warning.cpp
@@ -4,35 +4,26 @@
 #include <boost/math/common_factor.hpp>
 #include "fair_warning.hpp"
 
-int greatest_common_divisor(std::vector<int> numbers)
+int greatest_common_divisor(const std::vector<int>& numbers)
 {
-    std::vector<int>::iterator it = numbers.begin();
+    std::vector<int>::const_iterator it = numbers.begin();
     int gcd = *it;
-    for (it++; it != numbers.end() && gcd > 1; it++)
+    for (it++; it != numbers.end(); it++)
         gcd = boost::math::gcd(gcd, *it);
     return gcd;
 }
 
 int calculate_apocalypse(std::vector<int> events)
 {
-    const int max_event = *std::max_element(events.begin(), events.end());
-    int apocalypse = 0, max_gcd = 0;
-    for (int i = 0;;) {
-        std::vector<int> events_plus_i(events);
-        for (std::vector<int>::iterator it = events_plus_i.begin();
-             it != events_plus_i.end(); it++)
-            *it += i;
-
-        int gcd = greatest_common_divisor(events_plus_i);
-        if (gcd > max_gcd) {
-            max_gcd = gcd;
-            apocalypse = i;
-        }
-
-        if (i > max_event * 10)
-            break;
-
-        i += max_gcd;
-    }
-    return apocalypse;
+    /*
+     * This code is inspired by the submission of neal.wu, because I was
+     * unable to come up with a reasonably fast solution myself.
+     */
+    std::sort(events.begin(), events.end());
+    int min = events[0];
+    for (std::vector<int>::iterator it = events.begin(); it != events.end();
+         it++)
+        *it -= min;
+    int gcd = greatest_common_divisor(events);
+    return (gcd - min % gcd) % gcd;
 }
--- fair_warning/src/fair_warning.hpp
+++ fair_warning/src/fair_warning.hpp
@@ -1,6 +1,6 @@
 #include <vector>
 
-int greatest_common_divisor(std::vector<int> numbers);
+int greatest_common_divisor(const std::vector<int>& numbers);
 int calculate_apocalypse(std::vector<int> events);
 
 
