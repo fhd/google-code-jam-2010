623e0bf91de2e07876cf4ceb12c8657182564c4c
Felix H. Dahlke <fhd@ubercode.de>
2011-05-06 21:50:27 +0200

    Implemented I/O.

--- qualification/theme_park/src/main.cpp
+++ qualification/theme_park/src/main.cpp
@@ -17,8 +17,32 @@ void process(const std::string& input_file, const std::string& output_file)
    std::ifstream input(input_file.c_str());
    std::ofstream output(output_file.c_str());
    std::string line;
+   bool first_line = true;
+   int i = 1, rounds, capacity;
+
    std::getline(input, line); // Ignore the first line.
-   // TODO: Read input and write output
+   while (std::getline(input, line)) {
+      if (line.empty())
+         continue;
+
+      std::vector<std::string> elements;
+      boost::split(elements, line, boost::is_any_of(" "));
+
+      // Every problem consists of two lines
+      if (first_line) {
+          rounds = boost::lexical_cast<int>(elements[0]);
+          capacity = boost::lexical_cast<int>(elements[1]);
+          first_line = false;
+      } else {
+          std::queue<int> groups;
+          for (std::vector<std::string>::iterator it = elements.begin(); it != elements.end(); it++)
+              groups.push(boost::lexical_cast<int>(*it));
+          output << "Case #" << i++ << ": "
+                 << profit_from(rounds, capacity, groups)
+                 << std::endl;
+          first_line = true;
+      }
+   }
 }
 
 int main(int argc, char *argv[])
